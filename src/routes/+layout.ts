d8e816df6237139f223d5081d1ce1285fed0c685
Sean Lynch <techniq35@gmail.com>
2023-06-21 23:04:15 -0400

    Move user fetch to layout load()

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,1 +1,25 @@
+import { Github } from '$lib/github';
+
 export const ssr = false;
+
+export async function load({ data }) {
+  return {
+    ...data,
+    user: fetchUser(data.accessToken)
+  };
+}
+
+async function fetchUser(accessToken: string) {
+  const github = new Github(accessToken);
+  const { viewer } = await github.graphql<{ viewer: any }>(
+    `
+      query {
+        viewer {
+          login
+          name
+        }
+      }
+    `
+  );
+  return viewer;
+}
--- src/routes/repositories/+page.svelte
+++ src/routes/repositories/+page.svelte
@@ -1,24 +1,18 @@
 <script lang="ts">
-  // import gql from 'graphql-tag';
-  import { AppBar, ProgressCircle, ListItem, Overlay } from 'svelte-ux';
+	// import gql from 'graphql-tag';
+	import { AppBar, ListItem } from 'svelte-ux';
 
-  export let data;
+	export let data;
 </script>
 
 <AppBar title="Repositories" />
 
 <main class="p-4">
-  <div class="relative min-h-[56px]">
-    <!-- {#if $query.loading}
-      <Overlay center class="rounded">
-        <ProgressCircle />
-      </Overlay>
-    {/if} -->
-
-    {#if data}
-      {#each data.repositories.nodes as repo}
-        <ListItem title={repo.nameWithOwner} subheading={repo.description} />
-      {/each}
-    {/if}
-  </div>
+	<div class="relative min-h-[56px]">
+		{#if data}
+			{#each data.repositories.nodes as repo}
+				<ListItem title={repo.nameWithOwner} subheading={repo.description} />
+			{/each}
+		{/if}
+	</div>
 </main>
