76b0be50d8288d0a0014c319895e111f13f3c3c9
Sean Lynch <techniq35@gmail.com>
2024-03-01 18:32:44 -0500

    [Stars page] Add a day volume chart

--- src/routes/stars/+layout.svelte
+++ src/routes/stars/+layout.svelte
@@ -1,22 +1,25 @@
 <script lang="ts">
-  import { format as formatDate } from 'date-fns';
+  import { scaleBand, scaleTime } from 'd3-scale';
+  import { extent, flatRollup } from 'd3-array';
+  import { timeDay } from 'd3-time';
 
   import { mdiAccount, mdiDatabase, mdiPlay } from '@mdi/js';
 
   import { Button, Card, PeriodType, TextField, format } from 'svelte-ux';
-
-  import { goto } from '$app/navigation';
   import {
     Area,
     Axis,
+    Bars,
     Chart,
     Highlight,
     LinearGradient,
     Svg,
     Tooltip,
-    TooltipItem
+    TooltipItem,
+    Rule
   } from 'layerchart';
-  import { scaleTime } from 'd3-scale';
+
+  import { goto } from '$app/navigation';
 
   export let data;
 
@@ -37,6 +40,13 @@
       count: i + 1
     };
   });
+  // .slice(-50);
+
+  $: barChartData = flatRollup(
+    chartData,
+    (values) => values.length,
+    (d) => timeDay.ceil(d.starred_at)
+  );
 </script>
 
 <main>
@@ -64,32 +74,59 @@
     <Card
       title="{data.variables.owner}/{data.variables.repo}"
       subheading="{data.stargazers.length} stargazers"
-      class="h-[300px]"
     >
-      <Chart
-        data={chartData}
-        x="starred_at"
-        xScale={scaleTime()}
-        y="count"
-        yDomain={[0, null]}
-        yNice
-        padding={{ left: 36, bottom: 32, right: 24 }}
-        tooltip
-      >
-        <Svg>
-          <Axis placement="left" grid rule format="metric" />
-          <Axis placement="bottom" format={(d) => format(d, PeriodType.Month)} rule />
-          <LinearGradient class="from-secondary/50 to-secondary/0" vertical let:url>
-            <Area line={{ class: 'stroke-2 stroke-secondary' }} fill={url} tweened />
-          </LinearGradient>
-          <Highlight points={{ class: 'fill-secondary' }} lines />
-        </Svg>
+      <div class="h-[300px]">
+        <Chart
+          data={chartData}
+          x="starred_at"
+          xScale={scaleTime()}
+          y="count"
+          yDomain={[0, null]}
+          yNice
+          padding={{ left: 36, bottom: 32, right: 24 }}
+          tooltip
+        >
+          <Svg>
+            <Axis placement="left" grid rule format="metric" />
+            <Axis placement="bottom" format={(d) => format(d, PeriodType.Day)} rule />
+            <LinearGradient class="from-secondary/50 to-secondary/0" vertical let:url>
+              <Area line={{ class: 'stroke-2 stroke-secondary' }} fill={url} tweened />
+            </LinearGradient>
+            <Highlight points={{ class: 'fill-secondary' }} lines />
+          </Svg>
 
-        <Tooltip header={(data) => formatDate(data.starred_at, 'M/d/yyyy @ h:mm aa')} let:data>
-          <TooltipItem label="User" value={data.user.login} />
-          <TooltipItem label="Count" value={data.count} />
-        </Tooltip>
-      </Chart>
+          <Tooltip header={(data) => format(data.starred_at, PeriodType.DayTime)} let:data>
+            <TooltipItem label="User" value={data.user.login} />
+            <TooltipItem label="Count" value={data.count} />
+          </Tooltip>
+        </Chart>
+      </div>
+
+      <div class="h-[100px]">
+        <Chart
+          data={barChartData}
+          x={(d) => d[0]}
+          xScale={scaleBand()}
+          xDomain={timeDay.range(...extent(barChartData, (d) => d[0]))}
+          y={(d) => d[1]}
+          yDomain={[0, null]}
+          yNice
+          padding={{ left: 36, bottom: 32, right: 24 }}
+          tooltip
+        >
+          <Svg>
+            <Axis placement="left" grid format="metric" ticks={3} />
+            <!-- <Axis placement="bottom" grid format={(d) => format(d, PeriodType.Day)} /> -->
+            <Rule y class="stroke-surface-content/20" />
+            <Bars class="fill-secondary" />
+            <Highlight area />
+          </Svg>
+
+          <Tooltip header={(data) => format(data[0], PeriodType.Day)} let:data>
+            <TooltipItem label="Count" value={data[1]} />
+          </Tooltip>
+        </Chart>
+      </div>
     </Card>
 
     <slot />
