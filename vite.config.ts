44da8d11399f8b4c5224330e13325c182b550eca
Sean Lynch <techniq35@gmail.com>
2025-08-02 12:56:36 -0400

    Setup devtools-json and switch to typescript vite config

---
+++ vite.config.ts
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+import tailwindcss from '@tailwindcss/vite';
+import devtoolsJson from 'vite-plugin-devtools-json';
+
+export default defineConfig({ plugins: [tailwindcss(), sveltekit(), devtoolsJson()] });
